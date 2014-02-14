9d493c1ef1d0ebd2e0bf2c05cf0c0e3d32c8b7e9
Wil Clouser <clouserw@gmail.com>
2014-02-13 16:54:32 -0800

    clean up code

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from gevent.pywsgi import WSGIServer
-from pypodio2 import api as podio
 from werkzeug.contrib.cache import MemcachedCache
 
 import local
-from utils import calc_milestone
+from utils import calc_milestone, connect_podio, parse_podio
 
 cache = MemcachedCache([os.getenv('MEMCACHE_URL', 'localhost:11211')])
 
@@ -18,112 +17,37 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 
-    c = podio.OAuthClient(local.PODIO_CLIENT_ID,
-                          local.PODIO_CLIENT_SECRET,
-                          local.PODIO_USER,
-                          local.PODIO_PASS)
+    r = cache.get('api_progress')
+    if not r:
+        c = connect_podio()
+        r = c.Application.get_items(local.PODIO_PROGRESS_APPLICATION)
+        cache.set('api_progress', r, timeout=2 * 60)
 
-    r = c.Application.get_items(local.PODIO_PROGRESS_APPLICATION)
-
-
-# TODO add memcache
-    results = []
-
-    for i in r['items']:
-        o = {}
-
-        if request.args.get('extrasauce'):
-            o['podio_link'] = i['link']
-
-        o['name'] = i['title']
-
-        for f in i['fields']:
-
-
-            if f['type'] == 'text':
-                o[f['label']] = f['values'][0]['value']
-
-            if f['type'] == 'category':
-                o[f['label']] = f['values'][0]['value']['text']
-
-            if f['type'] == 'category' and len(f['values']) > 1:
-
-                o[f['label']] = []
-
-                for v in f['values']:
-                    o[f['label']].append(v['value']['text'])
-
-            if f['type'] == 'progress':
-                o[f['label']] = f['values'][0]['value']
-
-            if f['label'] == 'milestone_override':
-                o['dev_complete'] = calc_milestone(o['milestone_override'])
-
-            if f['type'] == 'embed':
-                o[f['label']] = f['values'][0]['embed']['url']
-
-        results.append(o)
+    results = parse_podio(r, extrasauce=request.args.get('extrasauce'))
 
     try:
         results.sort(key=lambda x: (x['status'],x['name']))
     except KeyError:
         pass
 
-
-
     return render_template('home.html', results=results)
+
 
 @app.route('/scoreboard')
 def scoreboard():
 
-    # TODO: make this a function!
-    c = podio.OAuthClient(local.PODIO_CLIENT_ID,
-                          local.PODIO_CLIENT_SECRET,
-                          local.PODIO_USER,
-                          local.PODIO_PASS)
+    r = cache.get('api_scoreboard')
+    if not r:
+        c = connect_podio()
+        r = c.Item.filter(local.PODIO_FEATURE_APPLICATION, {'limit': 50})
+        cache.set('api_scoreboard', r, timeout=2 * 60)
 
-    r = c.Item.filter(local.PODIO_FEATURE_APPLICATION, {'limit': 50})
+    results = parse_podio(r, extrasauce=request.args.get('extrasauce'))
 
-    results = []
-
-    for i in r['items']:
-        o = {}
-
-        if request.args.get('extrasauce'):
-            o['podio_link'] = i['link']
-
-        # set a default because we sort on it
-        o['Net Score'] = 0
-
-        # TODO: this whole thing should be a function to share with home()
-        for f in i['fields']:
-
-            if f['type'] == 'text':
-                o[f['label']] = f['values'][0]['value']
-
-            if f['type'] == 'category':
-                o[f['label']] = f['values'][0]['value']['text']
-
-            if f['type'] == 'category' and len(f['values']) > 1:
-
-                o[f['label']] = []
-
-                for v in f['values']:
-                    o[f['label']].append(v['value']['text'])
-
-            if f['type'] == 'progress':
-                o[f['label']] = f['values'][0]['value']
-
-            if f['label'] == 'milestone_override':
-                o['dev_complete'] = calc_milestone(o['milestone_override'])
-
-            if f['type'] == 'embed':
-                o[f['label']] = f['values'][0]['embed']['url']
-
-            if f['type'] == 'number':
-                o[f['label']] = int(float(f['values'][0]['value']))
-
-        results.append(o)
+    # We sort on "Net Score" so we need to make sure it's there
+    for i, item in enumerate(results):
+        if not 'Net Score' in item:
+            results[i]['Net Score'] = 0
 
     try:
         results.sort(key=lambda x: (x['Net Score']), reverse=True)
@@ -131,6 +55,7 @@ def scoreboard():
         pass
 
     return render_template('scoreboard.html', results=results)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
--- utils.py
+++ utils.py
@@ -3,12 +3,17 @@ import os
 import json
 import requests
 
+from pypodio2 import api as podio
 from werkzeug.contrib.cache import MemcachedCache
+
+import local
 
 cache = MemcachedCache([os.getenv('MEMCACHE_URL', 'localhost:11211')])
 
+
 def calc_milestone(url):
     # TODO: handle more than github
+    # TODO: This is broken right now.  Miscalculating somehow
 
     c = cache.get(url)
 
@@ -29,3 +34,54 @@ def calc_milestone(url):
         pass
 
     return ret
+
+
+def connect_podio():
+    c = podio.OAuthClient(local.PODIO_CLIENT_ID,
+                          local.PODIO_CLIENT_SECRET,
+                          local.PODIO_USER,
+                          local.PODIO_PASS)
+    return c
+
+
+def parse_podio(juicydata, extrasauce=None):
+    """This converts the awesome-yet-very-verbose podio API output into
+    something that is easier to use in our templates.
+    """
+    results = []
+
+    for i in juicydata['items']:
+        o = {}
+
+        o['name'] = i['title']
+
+        if extrasauce:
+            o['podio_link'] = i['link']
+
+        for f in i['fields']:
+
+            if f['type'] == 'text':
+                o[f['label']] = f['values'][0]['value']
+
+            if f['type'] == 'category':
+                o[f['label']] = f['values'][0]['value']['text']
+
+            if f['type'] == 'category' and len(f['values']) > 1:
+
+                o[f['label']] = []
+
+                for v in f['values']:
+                    o[f['label']].append(v['value']['text'])
+
+            if f['type'] == 'progress':
+                o[f['label']] = f['values'][0]['value']
+
+            if f['type'] == 'embed':
+                o[f['label']] = f['values'][0]['embed']['url']
+
+            if f['type'] == 'number':
+                o[f['label']] = int(float(f['values'][0]['value']))
+
+        results.append(o)
+
+    return results
